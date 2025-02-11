da3b66a8270f7388221721fa53980e92e230dbb5
Chris Campbell <cacampbell@ucdavis.edu>
2025-02-11 11:29:15 -0800

    feat: handle json
    Signed-off-by: Chris Campbell <cacampbell@ucdavis.edu>

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express"
 
-const app = express();
+const app = express().use(express.json())
 const port = process.env.PORT || 3001
 
-app.get("/", (req, resp) => {
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+app.get("/", (req: Request, resp: Response) => {
     resp.send("OK")
 })
