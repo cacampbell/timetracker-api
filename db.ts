f304051ef0c6de302c3adb4d78116169e2155935
Chris Campbell <cacampbell@ucdavis.edu>
2025-02-11 16:44:14 -0800

    fix: config, build whoopsies
    Signed-off-by: Chris Campbell <cacampbell@ucdavis.edu>

--- db.ts
+++ db.ts
@@ -3,12 +3,7 @@ import { User } from "./models/user"
 import { Timesheet } from "./models/timesheet"
 import { LineItem } from "./models/lineitem"
 
-User.hasMany(Timesheet)
-Timesheet.belongsTo(User)
-Timesheet.hasMany(LineItem)
-LineItem.belongsTo(Timesheet)
-
-export default new Sequelize({
+const sequelize = new Sequelize({
     dialect: "postgres",
     host: process.env.DB_HOSTNAME || "localhost",
     username: process.env.DB_USER || "postgres",
@@ -17,3 +12,10 @@ export default new Sequelize({
     logging: false,
     models: [User, Timesheet, LineItem]
 })
+
+User.hasMany(Timesheet)
+Timesheet.belongsTo(User)
+Timesheet.hasMany(LineItem)
+LineItem.belongsTo(Timesheet)
+
+export default sequelize;
