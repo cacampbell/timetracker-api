001a5f846e315e2bf6c0a35ac6cb2289085239be
Chris Campbell <cacampbell@ucdavis.edu>
2025-02-13 18:03:00 -0800

    fix: cors for localhost
    Signed-off-by: Chris Campbell <cacampbell@ucdavis.edu>

--- index.ts
+++ index.ts
@@ -10,8 +10,16 @@ import { config as dotenvConfig } from "dotenv";
 dotenvConfig()
 import conn from "./db"
 
+// CORS
+import cors from "cors"
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"]
+}
+
 // Handle JSON on PORT
-const app = express().use(express.json())
+const app = express().use(express.json()).use(cors(corsOptions))
 const port = process.env.PORT || 3001
 
 // Status
